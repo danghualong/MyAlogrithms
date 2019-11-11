539e76984bfc035aa3e319fdeb0ac1a4fdc09393
danghualong <dhl_345@qq.com>
2019-11-11 22:04:30 +0800

    增加组合实现的两种方式

--- alog/composition.py
+++ alog/composition.py
@@ -1,4 +1,5 @@
 class Composition(object):
+    #利用数组中的数，每个值可以使用多次，只要加和等于目标值即可
     def combinationSum(self,arr,target):
         arr.sort()
         seq=[]
@@ -17,6 +18,23 @@ class Composition(object):
                 self.combine(arr,target-arr[i],i,seq,result)
                 seq.pop()
 
+
+    @staticmethod
+    def addSeq(result,seq):
+        # for tmpSeq in result:
+        #     if(Composition.isSame(tmpSeq,seq)):
+        #         return
+        result.append(seq[:])
+
+    @staticmethod       
+    def isSame(tmpSeq,seq):
+        if(len(tmpSeq)!=len(seq)):
+            return False
+        for i in range(len(seq)):
+            if(tmpSeq[i]!=seq[i]):
+                return False
+        return True  
+    #从数组中算出所有加和等于目标值的子序列集合(循环实现)
     def combinationSum2(self,arr,target):
         arr.sort()
         len1=len(arr)
@@ -46,27 +64,32 @@ class Composition(object):
                 if(j<len1-1): 
                     break
             j+=1
-        return result
-    
-    @staticmethod
-    def addSeq(result,seq):
-        for tmpSeq in result:
-            if(Composition.isSame(tmpSeq,seq)):
-                return
-        result.append(seq[:])
+        return result  
+    #从数组中算出所有加和等于目标值的子序列集合(递归实现)
+    def compose(self,arr,target):
+        seq=[]
+        index=0
+        self.backtrack(arr,target,seq,index)
 
-    @staticmethod       
-    def isSame(tmpSeq,seq):
-        if(len(tmpSeq)!=len(seq)):
-            return False
-        for i in range(len(seq)):
-            if(tmpSeq[i]!=seq[i]):
-                return False
-        return True       
+    def backtrack(self,arr,target,seq,index):
+        if(target==0):
+            print(seq)
+            return
+        if(index>=len(arr)):
+            return
+        if(target>=arr[index]):
+            seq.append(arr[index])
+            #包含当前值时，再试探下一个值
+            self.backtrack(arr,target-arr[index],seq,index+1)
+            seq.pop()
+            #不包含当前值时，再试探下一个值
+            self.backtrack(arr,target,seq,index+1)
+        else:#如果超出target,则试探下一个值
+            self.backtrack(arr,target,seq,index+1)
 
 
 arr=[4,4,2,1,4,2,2,1,3]
 comp=Composition()
-# print(comp.combinationSum2(arr,6))
-arr=[4,4,2,1,4,2,2,1,3]
 print(comp.combinationSum(arr,6))
+comp.compose(arr,6)
+print(comp.combinationSum2(arr,6))
