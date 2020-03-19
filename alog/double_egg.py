0da0293900f66ab2c6501bb3edb3a30e4ef01e77
danghualong <dhl_345@qq.com>
2020-03-19 18:21:33 +0800

    修改逻辑错误

--- alog/double_egg.py
+++ alog/double_egg.py
@@ -2,21 +2,24 @@
 import math
 # 动态规划实现双蛋问题
 def calc(m,n):
-    arr=[[0 for j in range(n)] for i in range(m)]
-    for i in range(m):
-        arr[i][0]=i+1
-    for j in range(n):
-        arr[0][j]=1
-    for i in range(1,m):
-        for j in range(1,n):
+    arr=[[0 for j in range(n+1)] for i in range(m+1)]
+    for i in range(m+1):
+        arr[i][1]=i
+    for j in range(1,n+1):
+        arr[1][j]=1
+    for i in range(2,m+1):
+        for j in range(2,n+1):
             minNum=math.inf
             for k in range(i):
                 # if the egg is not broken
-                t1=1+arr[i-1-k][j]
+                t1=arr[i-k-1][j]
                 # if the egg is broken
                 t2=arr[k][j-1]
+                # print('i:{2},j:{3},k:{4},t1:{0},t2:{1}'.format(t1+1,t2+1,i,j,k))
+                    
                 # get the maximum
                 num=t1 if t1>t2 else t2
+                num+=1
                 # get the minimum of the nums in different k
                 if(num<minNum):
                     minNum=num
@@ -25,4 +28,4 @@ def calc(m,n):
     
 
 
-calc(100,20)
+calc(100,10)
